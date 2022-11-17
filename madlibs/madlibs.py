16b4c939426fa78fd60cab9686e250aa38da4975
etimbuk9 <etimbuk9@yahoo.co.uk>
2022-11-17 17:12:17 +0100

    madlibs done

---
+++ madlibs/madlibs.py
@@ -0,0 +1,10 @@
+## String Concatenation
+
+adj = input('Adjective: ')
+verb1 = input('Verb: ')
+verb2 = input('Verb: ')
+famous_person = input('Famous Person: ')
+
+madlib = f'Computer programming is so {adj}! It makes me to excited all the time because I love to {verb1}. Stay Hydrated and {verb2} like you are {famous_person}.'
+
+print(madlib)
