3f1211d5826141303cf9ab67afb909cccccc585b
etimbuk9 <etimbuk9@yahoo.co.uk>
2022-11-17 19:54:28 +0100

    player defined

--- tic-tac-toe/player.py
+++ tic-tac-toe/player.py
@@ -3,4 +3,21 @@ import random
 
 class Player:
     def __init__(self, letter):
+        self.letter = letter
+    
+    def get_move(self, game):
+        pass
+
+class RandomComputerPlayer(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
+    
+    def get_move(self, game):
+        pass
+
+class HumanPlayer(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
+    
+    def get_move(self, game):
         pass
